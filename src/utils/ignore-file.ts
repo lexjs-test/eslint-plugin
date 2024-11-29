eac6bda983a5c84203eb2e5cc6cfdc281cd20684
Lex Borisoff <lex@borisoff.dev>
2024-11-28 19:51:53 -0500

    chore: causing a build error

--- src/utils/ignore-file.ts
+++ src/utils/ignore-file.ts
@@ -2,7 +2,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile, type FlatConfig } from '@eslint/compat';
 
-export function ignoreFile(file: string, meta: ImportMeta): FlatConfig {
+export function ignoreFile(file: string, meta: ImportMeta): string {
   const __filename = fileURLToPath(meta.url);
   const __dirname = path.dirname(__filename);
   const gitignorePath = path.resolve(__dirname, file);
